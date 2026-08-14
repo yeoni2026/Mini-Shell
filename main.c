57df9090bb873548eb17a2ae32b37e678efed867
yeoni <yeoni2026@gmail.com>
2026-08-14 13:51:52 +0900

    파이프 구현 - 그러나 fd[1]과 fd[0] 사이의 연결 오류 있음

--- main.c
+++ main.c
@@ -9,7 +9,9 @@
 
 #define COMMAND_SIZE 100
 #define ARGV_SIZE 10
+#define IDX(i, j) (i * 2 + j)
 
+void redirection(int argc, char *argv[]);
 
 int main(void){
     char command[COMMAND_SIZE];
@@ -47,73 +49,131 @@ int main(void){
             continue;
         }
 
+        int error_flag = 0;
+        int command_num = 0;
+        int start_point = 0;
+        int fd[50];
+        int status;
+
+        for (int j = 0; j < i; j++){
+            if (strcmp(argv[j], "|") == 0){
+                if (j == 0 || j == i){
+                    printf("Missing command\n");
+                    error_flag = 1;
+                    break;
+                }
+                argv[j] = NULL;
+                pipe(fd + 2 * command_num);
+                
+                pid_t pid = fork();
+                if (pid == -1){
+                    printf("Fork failed\n");
+                    continue;
+                }
+                if (pid == 0){
+                    if (command_num != 0) {
+                        dup2(fd[IDX(command_num - 1, 0)], 0);
+                        close(fd[IDX(command_num - 1, 0)]);
+                        close(fd[IDX(command_num - 1, 1)]);
+                    }
+                    int save_out = dup(1);
+                    dup2(fd[IDX(command_num, 1)], 1);
+                    close(fd[IDX(command_num, 1)]);
+                    close(fd[IDX(command_num, 0)]);
+
+                    execvp(argv[start_point], argv + start_point);
+                    dup2(save_out, 1);
+                    printf("Command execution failed\n");
+                    exit(1);
+                }
+                if (command_num != 0) close(fd[IDX(command_num - 1, 0)]);
+                close(fd[IDX(command_num, 1)]);
+                start_point = j + 1;
+                command_num++;
+            }
+        }
+        if (error_flag){
+            while (wait(&status) > 0);
+            continue;
+        }
+
         pid_t pid = fork();
         if (pid == -1){
             printf("Fork failed\n");
             continue;
         }
         if (pid == 0){
-            //IO 리다이렉션 
-            int error_flag = 0;
-            int fileout_flag = 0;
-            int filein_flag = 0;
-            int fd_out, fd_in;
-            char message[100]; //바로바로 printf 출력을 하지 않고 message에 문구를 담아 이후 error_flag 처리에서 출력하는 이유: for 반복문 내에서 리다이렉션이 즉각적으로 이루어지기에 message가 표준출력(stdout)으로 출력되지 않을 위험이 있기 때문
-
-            for (int j = 0; j < i; j++){
-                if (strcmp(argv[j], ">") == 0){
-                    if (j + 1 == i){
-                        strcpy(message, "Missing output file");
-                        error_flag = 1;
-                        break;
-                    }
-                    if (fileout_flag){
-                        strcpy(message, "too many redirections");
-                        error_flag = 1;
-                        break;
-                    }
-
-                    fd_out = open(argv[j + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                    dup2(fd_out, 1);
-                    close(fd_out);
-                    fileout_flag = 1;
-                    argv[j] = NULL;
-                }
-                else if (strcmp(argv[j], "<") == 0){
-                    if (j + 1 == i){
-                        strcpy(message, "Missing input file");
-                        error_flag = 1;
-                        break;
-                    }
-                    if (filein_flag){
-                        strcpy(message, "too many redirections");
-                        error_flag = 1;
-                        break;
-                    }
-
-                    fd_in = open(argv[j + 1], O_RDONLY);
-                    if (fd_in == -1){
-                        strcpy(message, "File not exist");
-                        error_flag = 1;
-                        break;
-                    }
-                    dup2(fd_in, 0);
-                    close(fd_in);
-                    filein_flag = 1;
-                    argv[j] = NULL;
-                }
+            if (command_num) {
+                dup2(fd[IDX(command_num - 1, 0)], 0);
+                close(fd[IDX(command_num - 1, 0)]);
+                close(fd[IDX(command_num - 1, 1)]);
             }
-            if (error_flag) {
-                printf("%s\n", message);
-                exit(1);
-            }
-            execvp(argv[0], argv);
+            else redirection(i, argv + start_point);
+            execvp(argv[start_point], argv + start_point);
             printf("Command execution failed\n");
             exit(1);
         }
         else {
-            int status;
-            waitpid(pid, &status, 0); //자식 프로세스가 exit되며 부모 프로세스가 SIGCHLD 시그널을 받을때까지 blocked
+            while (wait(&status) > 0);
         }
+    }
+}
+
+void redirection(int argc, char *argv[]){
+    //IO 리다이렉션 
+    int error_flag = 0;
+    int fileout_flag = 0;
+    int filein_flag = 0;
+    int fd_out, fd_in, save_out;
+    char message[100]; //바로바로 printf 출력을 하지 않고 message에 문구를 담아 이후 error_flag 처리에서 출력하는 이유: for 반복문 내에서 리다이렉션이 즉각적으로 이루어지기에 message가 표준출력(stdout)으로 출력되지 않을 위험이 있기 때문
+
+    for (int j = 0; j < argc; j++){
+        if (strcmp(argv[j], ">") == 0){
+            if (j + 1 == argc){
+                strcpy(message, "Missing output file");
+                error_flag = 1;
+                break;
+            }
+            if (fileout_flag){
+                strcpy(message, "too many redirections");
+                error_flag = 1;
+                break;
+            }
+
+            fd_out = open(argv[j + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            save_out = dup(1);
+            dup2(fd_out, 1);
+            close(fd_out);
+            fileout_flag = 1;
+            argv[j] = NULL;
+        }
+        else if (strcmp(argv[j], "<") == 0){
+            if (j + 1 == argc){
+                strcpy(message, "Missing input file");
+                error_flag = 1;
+                break;
+            }
+            if (filein_flag){
+                strcpy(message, "too many redirections");
+                error_flag = 1;
+                break;
+            }
+
+            fd_in = open(argv[j + 1], O_RDONLY);
+            if (fd_in == -1){
+                strcpy(message, "File not exist");
+                error_flag = 1;
+                break;
+            }
+            dup2(fd_in, 0);
+            close(fd_in);
+            filein_flag = 1;
+            argv[j] = NULL;
+        }
+    }
+    if (error_flag) {
+        dup2(save_out, 1);
+        printf("%s\n", message);
+        exit(1);
     }
 }
